9451eac3eabd5a91b96da61cfd606bcc310bdea8
hugoh92 <hugoholiveira45@gmail.com>
2016-04-14 21:15:03 -0300

    Update barbeirocara.c

--- barbeirocara.c
+++ barbeirocara.c
@@ -5,88 +5,94 @@
 #include<sys/ipc.h>
 #include<semaphore.h>
 
-#define Tamanho_Fila 5
-#define Numero_Cabines 2
+#define Tamanho_Fila 10
+#define Numero_Cabines 5
+#define Tipos_Veiculos 7
 
-sem_t Novo_Carro; // Representa que um novo Carro chegou
-sem_t Filas[Tamanho_Fila]; // Representa as vagas disponiveis no pedagio
-sem_t Cabines[Numero_Cabines]; // Representa a quantidade de cabines
-sem_t Sinal[Numero_Cabines]; // Representa se uma Cabine esta liberada para receber um novo carro
+sem_t sem_Veiculos; //representa os veiculos
+sem_t sem_Fila; //representa a fila de veiculos
+sem_t sem_Cabine[Numero_Cabines]; // representa as cabines do pedagio
+sem_t sem_Cabine_check[Numero_Cabines]; // represta se carro passou ou nao
+sem_t sem_Veiculos_Fila[Numero_Cabines]; //representa o num de carros na fila
 
-int i,N,Numero_da_Cabine, Carros_Rodovia=0, N_Cabine[Numero_Cabines]; // variaveis de controle
+sem_t sem_escreve, sem_ler;
+int visor, amb=0, viat=0;
 
-pthread_t Cabine,Carro ; //trheads das cabines e dos carros
+void* f_cabine(void * v){
+	int id = *(int*)v;
 
-void * cabine(void * N);
-void * carro();
-void * carro_na_cabine(int Numero_da_Cabine);
-
-void * cabine(void * N){
-	while(1){
-		Numero_da_Cabine = * (int *) N; // Passa numero da cabine para inteiro e recebe o parametro N
-		for (i=0;i<Tamanho_Fila;i++){ // pecorre tamanho da fila,   video ta < mas acho que é <=
-			if  (sem_trywait(&Filas[i]) == 0){ // verifica se a fila está vazia, 
-				printf(" Pedágio Vazio\n");
-				sem_wait(&Novo_Carro); // Esperando novo Carro
-				sem_post(&Sinal[Numero_da_Cabine]); // Sinal de Cabine livre
-				sem_wait(&Cabines[Numero_da_Cabine]);// Libera a Cabine
-			}
-			else{
-				sem_post(&Novo_Carro); // libera um Carro da fila
-				sem_post(&Filas[i]); // Libera um posicão da fila para chegada de um Novo carro	
-				sem_wait(&Sinal[Numero_da_Cabine]); // Avisa que a está Cabine Ocupada
-				sem_wait(&Cabines[Numero_da_Cabine]); // Bloqueia Cabine
-				carro_na_cabine(Numero_da_Cabine);
-			}
+	while(1) {
+		sem_wait(&sem_escreve);  // espera veiculo
+		visor = id;  //mostra que está livre
+		sem_post(&sem_ler); //avisa que esta acordado
+		sem_wait(&sem_Veiculos_Fila[id]); //espera veiculo passar
+		if (Tipos_Veiculos == 1){
+			usleep(random()1000*1); //passa na cabine
+		}else if (Tipos_Veiculos==2){
+			usleep(random()1000*2);
 		}
+		else if (Tipos_Veiculos==3){
+
+		}
+		// aqui tem que ter um if (Tipos_Veiculos == 1){} de acordo com o tipo de veiculo o tmepo muda
+		usleep(random()1000*1); //passa na cabine
+		sempost($sem_Cabine_check[id]) //avisa que carro passou
 	}
+
 }
 
-void * carro(){
-	for(i=0;i<=Tamanho_Fila;i++){
-		if (sem_trywait(&Filas[i]) != 0){ // verifica se existe posições vazias na fila
-			for (N=0;N<=Numero_Cabines;N++){ // peccore as cabines
-				if (sem_trywait(&Sinal[N]) == 0){ // Verifica se todas as cabines está cheia
-  					sem_wait(&Filas[i]); // Aguarda na fila
-				}
-				else{ // Verifica alguma Cabine Vazia
-					printf("Cabine %d Livre.\n", N +1);
-					sem_post(&Novo_Carro); // Libera um novo carro
-					sem_post(&Filas[i]); // Libera um posição na fila
-					sem_wait(&Cabines[N]); // bloqueia a cabine
-					sem_wait(&Sinal[N]); // Avisa que cabine está ocupada	
-				}			
-			}		
-		}
-		else{
-			Carros_Rodovia = Carros_Rodovia+1;
-			if (Carros_Rodovia == Tamanho_Fila){ // video ta == mas acho que é <=
-				printf("Pedagio Lotado.\n");			
-			}	
-		}
-	}
-	Carros_Rodovia =0;
-}
+void * f_veiculo(void* v) { //veiculos
+	int id = *(int*) v;
+	int minha_Fila;
 
-void * carro_na_cabine(int Numero_da_Cabine){
-	printf("Carro na %d Cabine.\n",Numero_da_Cabine+1);
-}
+	usleep(random()%700+4);
+	if (sem_trywait(&sem_Fila)==0) {
+		printf("Veiculo tipo %d chegou na fila");
+		sem_wait(&sem_ler); //espera cabine indicar que estar dormindo
+		minha_Fila = visor;  //olha qual cabine esta dormindo
+		sem_post(&sem_escreve); //aisa que esta indo ser atendido
+		sem_wait(&sem_Cabine)[minha_Fila]); //espera a cabine acordar
+		printf("(Veiculo %d está na cabine %d \n",id,minha_Fila ); 
+		sem_post(&sem_Veiculos_Fila[minha_Fila]); //avisa que veiculo esta na fila
+		sem_post(&sem_Fila) //deixa a fila
+		sem_wait(&sem_Cabine_check[minha_Fila]);  //espera cabine abrir cancela
+		sem_post(&sem_Cabine[minha_Fila]); //deixa cabine
+		printf("Veiculo %d foi atendido.\n",id );
+	} else
+		printf("veiculo %d n  tem lugar na fila)\n",id ); //nao tem lugar
+		return NULL;
+	}
 
-int main(){
-	//inicialização dos semaforos
-	sem_init(&Novo_Carro,1,0);
-	for (i=0;i<Numero_Cabines;i++){
-		sem_init(&Filas[i],1,0);
-		sem_init(&Sinal[i],1,0);
+	int main() {
+		pthread_t tht_veiculos[Tamanho_Fila], thr_cabines[Numero_Cabines];
+		int i, id_vc[Tamanho_Fila], id_cab[Numero_Cabines];
+
+		sem_init(&sem_Fila,0,10);				//fila
+		sem_init(&sem_escreve,0,1);				
+		sem_init(&sem_ler,0,0);
+
+		for (i =0;i < Numero_Cabines;i++){			//inicia semaforo
+			sem_init(&sem_Cabine[i],0,1);
+			sem_init(&sem_Veiculos_Fila[i],0,0);
+			sem_init(&sem_Cabine_check[i],0,0)
+		}
+
+		for (i=0;i< Tamanho_Fila;i++) {  //cria fila
+			id_vc[i] = i;
+			pthread_create(&thr_veiculos[i], NULL, f_veiculo, (void*) &id_vc[i]);
+		}
+		for (i=0;i< Tamanho_Cabines;i++) {  //cria cabines
+			id_cab[i] = i;
+			pthread_create(&thr_cabines[i], NULL, f_cabine, (void*) &id_vc[i]);
+		}
+
+		for (i=0; i < Tamanho_Fila;i++)
+			pthread_join(thr_veiculos[i],NULL);
+		}
+		return 0;
 	}
-	// Criação das Threads
-	for (i=0; i< Numero_Cabines;i++){
-		N_Cabine[i]=i;
-		pthread_create(&Cabine,NULL,(void *)cabine,&N_Cabine[i]);
+
+
 	}
-	// Criação de Carros					
-	while(1){
-		pthread_create(&Carro,NULL,(void *)carro,NULL);
-	}
-	return 0;
+
 }
